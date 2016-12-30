994ed4035868bee5a4c8e81eb7fd95c17de9780f
xuwei-k <6b656e6a69@gmail.com>
2016-12-31 00:01:14 +0900

    sbt 0.13.13

--- src/main/scala/Harness.scala
+++ src/main/scala/Harness.scala
@@ -13,33 +13,34 @@ object Harness extends Plugin {
       "Run a named launchconfig, with parameters")
   val conscriptSettings: Seq[Def.Setting[_]] = Seq(
     resolvers += Classpaths.typesafeReleases,
-    libraryDependencies <+= (sbtVersion) { sbtv =>
-      "org.scala-sbt" % "launcher-interface" % sbtv % "provided"},
-    conscriptBase <<= (sourceDirectory in Compile)(_ / "conscript"),
-    conscriptOutput <<= target(_ / "conscript"),
-    conscriptBoot <<= conscriptOutput(_ / "boot"),
-    csWrite <<= csWriteTask,
-    csRun <<= csRunTask,
-    csRun <<= csRun.dependsOn(csWrite, publishLocal),
+    libraryDependencies += {
+      "org.scala-sbt" % "launcher-interface" % "1.0.0" % "provided"},
+    conscriptBase := ((sourceDirectory in Compile).value / "conscript"),
+    conscriptOutput := (target.value / "conscript"),
+    conscriptBoot := (conscriptOutput.value / "boot"),
+    csWrite := csWriteTask.value,
+    csRun := csRunTask.evaluated,
+    csRun := csRun.dependsOn(csWrite, publishLocal),
     (aggregate in csRun) := false
   )
   private def configs(path: File) = (path ** "launchconfig").get
   private def configName(path: File) =
     new File(path.getParent).getName
   def csWriteTask =
-    (conscriptBase, conscriptOutput, conscriptBoot) map {
-      (base, output, boot) =>
-        IO.delete(output)
-        IO.copyDirectory(base, output)
-        configs(output).map { path =>
-          IO.append(path, 
-            """
-            |[boot]
-            |  directory: %s
-            |""".stripMargin.format(boot)
-          )
-        }
-      ()
+    Def.task[Unit] {
+      val base = conscriptBase.value
+      val output = conscriptOutput.value
+      val boot = conscriptBoot.value
+      IO.delete(output)
+      IO.copyDirectory(base, output)
+      configs(output).map { path =>
+        IO.append(path, 
+          """
+          |[boot]
+          |  directory: %s
+          |""".stripMargin.format(boot)
+        )
+      }
     }
   def csRunTask = Def.inputTask {
     import sbt.Process._
